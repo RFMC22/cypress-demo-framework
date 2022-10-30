1273d57ba864c881fc8d6b2218a9b7562e3ffd32
Renee Magallanes <rmagallc@emeal.nttdata.com>
2022-10-30 13:47:02 -0500

    arreglando bugs

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -61,15 +61,3 @@ module.exports = (on, config) => {
   }); //For running sql query
 };
 
-const { lighthouse, prepareAudit } = require('@cypress-audit/lighthouse')
-
-module.exports = (on, config) => {
-  on('before:browser:launch', (browser = {}, launchOptions) => {
-    prepareAudit(launchOptions)
-  })
-
-  on('task', {
-    lighthouse: lighthouse(), // calling the function is important
-  })
-}
-
